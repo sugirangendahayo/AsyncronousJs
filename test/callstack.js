61d398c4d71701edfa27d8293a6d164eccc900fa
sugirangendahayo <Sugirangendahayo835@gmail.com>
2026-01-31 18:12:18 +0200

    callstack flow demo

---
+++ test/callstack.js
@@ -0,0 +1,35 @@
+console.log('A');
+
+setTimeout(() => console.log('B'), 0);
+
+Promise.resolve()
+    .then(() => console.log('C'))
+    .then(() => console.log('D'));
+
+setTimeout(() => {
+    console.log('E');
+    Promise.resolve().then(() => console.log('F'));
+}, 0);
+
+Promise.resolve().then(() => {
+    console.log('G');
+    setTimeout(() => console.log('H'), 0);
+});
+
+console.log('I');
+```
+
+<details>
+<summary>💡 Solution & Explanation</summary>
+
+**Output:**
+```
+A
+I
+C
+G
+D
+B
+E
+F
+H
