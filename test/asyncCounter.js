12b80d4cff00aac3f787614aa05f4d2ead6a3c1c
sugirangendahayo <Sugirangendahayo835@gmail.com>
2026-02-01 13:22:34 +0200

    retried asyn with counter delays

---
+++ test/asyncCounter.js
@@ -0,0 +1,22 @@
+async function asyncCounter() {
+  let result = [];
+  for (let i = 1; i <= 5; i++) {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+    result.push(i);
+    console.log("Current number: ", i);
+    console.log("Current result: ", result);
+  }
+  return result;
+}
+async function runCounters() {
+  try {
+    const result = await asyncCounter();
+    console.log(result);
+  } catch (error) {
+    console.error("Error: ", error);
+    
+  }
+}
+runCounters()
