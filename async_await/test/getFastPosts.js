53cc47edad59cd9d3866cca0d817bd6177bd6e04
sugirangendahayo <Sugirangendahayo835@gmail.com>
2026-02-06 14:20:58 +0200

    feat:used array for passing urls

--- async_await/test/getFastPosts.js
+++ async_await/test/getFastPosts.js
@@ -1,14 +1,25 @@
 async function getFastPosts() {
-  const response = await Promise.any([
-    fetch("https://dummyjson.com/posts"),
-    fetch("https://this-may-not-exist.com/posts"),
-    fetch("https://jsonplaceholder.typicode.com/posts"),
-  ]);
+  const urls = [
+    "https://dummyjson.com/posts",
+    "https://this-may-not-exist.com/posts",
+    "https://jsonplaceholder.typicode.com/posts",
+  ];
+
+
+  const fetchPromises = urls.map((url) => fetch(url));
+  console.log("Fetch urls using array: ", fetchPromises);
+  
+ 
+  const response = await Promise.any(fetchPromises);
 
   const data = await response.json();
   return data;
 }
 
-getFastPosts().then((posts) => {
-  console.log(posts);
-});
+getFastPosts()
+  .then((posts) => {
+    console.log(posts);
+  })
+  .catch((err) => {
+    console.log("All requests failed", err);
+  });
